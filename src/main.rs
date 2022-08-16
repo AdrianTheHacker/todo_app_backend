4d010d08aed582d92179037266a83294a7e17f2a
Gravitysause <adriantarantino2006@gmail.com>
2022-08-16 14:11:27 -0400

    Rewriting get_todo_list()

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,39 @@
 #[macro_use] extern crate rocket;
 
-extern crate rustc_serialize;
-use rustc_serialize::json::Json;
+use serde_json::{Number, Value};
 
 use std::fs::File;
 use std::io::Read;
 
-#[warn(dead_code)]
 #[get("/hello/<name>")]
 fn say_hi(name: &str) -> String {
+    // IDK why I put this here
+    // consider it to be an easter egg
+
     format!("Hello, {}!", name)
 }
 
 #[get("/get_todo_list/<user>")]
 fn get_todo_list(user: &str) -> String {
-    let file_path = format!(r"src\Data\{}.json", user);
-    let mut file = File::open(file_path).unwrap();
-    let mut data = String::new();
+    // Gets the todo list of a given user.
 
-    file.read_to_string(&mut data).unwrap();
+    // Throws an error that the file isn't found 
+    // if the user doesn't have a todo list.
 
-    let json = Json::from_str(&data).unwrap();
-    format!("{}", json.find_path(&["Todo"]).unwrap())
+    let file_path = format!(r"src\Data\{}.json", user);         // Opens the json file for the given user
+    let text = std::fs::read_to_string(&file_path).unwrap();    // Turns contents of the file into a String
+    text                                                        // Returns the file contents as a String
+                                                                // The contents will be printed to the screen
 }
+
+// #[post("/add_event/<user>")]
+// fn add_event(user: &str) -> String {
+//     // Adds event to users todo list
+//     // Returns users todo list
+// }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![find_data_value])
+    rocket::build().mount("/", routes![say_hi])
                    .mount("/", routes![get_todo_list])
 }
