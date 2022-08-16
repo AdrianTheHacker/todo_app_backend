34178cc6fa49ce490a0edd98de3fda4d201317df
Gravitysause <adriantarantino2006@gmail.com>
2022-08-16 13:01:03 -0400

    Completing get_todo_list() function

--- src/main.rs
+++ src/main.rs
@@ -12,31 +12,20 @@ fn say_hi(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
-#[get("/find_data/<name>/<data_key>")]
-fn find_data_value(name: &str, data_key: &str) -> String {
-    let file_path = format!(r"src\Data\{}.json", name);
-    println!("{}", file_path);
+#[get("/get_todo_list/<user>")]
+fn get_todo_list(user: &str) -> String {
+    let file_path = format!(r"src\Data\{}.json", user);
     let mut file = File::open(file_path).unwrap();
     let mut data = String::new();
 
     file.read_to_string(&mut data).unwrap();
 
     let json = Json::from_str(&data).unwrap();
-    format!("{}", json.find_path(&[data_key]).unwrap())
-}
-
-fn get_todo_list(user: &str) -> String {
-    let file_path = format!(r"src\Data\{}.json", name);
-    let mut file = File::open(file_path).unwrap();
-    let mut data = String::new();
-
-    file.read_to_string(&mut date).unwrap();
-
-    let json = Json::from_str(&data).unwrap();
-    format!("{}", json.find_path(&[data_key]).unwrap())
+    format!("{}", json.find_path(&["Todo"]).unwrap())
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![find_data_value])
+                   .mount("/", routes![get_todo_list])
 }
