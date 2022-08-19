bf7e2778aca4b13042d802990967fcd9cbf3d2d7
Gravitysause <adriantarantino2006@gmail.com>
2022-08-19 12:11:48 -0400

    Completing delete_event

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn get_all_events(user: &str) -> String {
 }
 
 #[get("/add_event/<user>/<event>")]
-fn add_event(user: &str, event: &str) {
+fn add_event(user: &str, event: &str) -> String {
     // Creates new events for a given user.
 
     let file_path = format!(r"src\Data\{}.json", user);     // Opens the JSON file for the given user.
@@ -33,13 +33,38 @@ fn add_event(user: &str, event: &str) {
     user_data.Todo.push(event.to_string());                              // Adds new event to User struct todo vec.
 
     let json = serde_json::to_string_pretty(&user_data).unwrap();        // Converts User struct into JSON.
-    fs::write(file_path, json).expect("Unable to write file");      // Writes new JSON to the users JSON file.
+    fs::write(file_path, json).expect("Unable to write to file");        // Writes new JSON to the users JSON file.
 
     println!("Successfully added '{}' to {}'s todo list", event, user);
+    return format!("Successfully added '{}' to {}'s todo list", event, user);
+}
+
+#[get("/delete_event/<user>/<event>")]
+fn delete_event(user: &str, event: &str) -> String {
+    let file_path = format!(r"src\Data\{}.json", user);
+    let text = fs::read_to_string(&file_path).unwrap();
+
+    let mut user_data: User = serde_json::from_str(&text).unwrap();
+
+    for list_index in user_data.Todo.len()..0 {
+
+        println!("User Data: {}", user_data.Todo[list_index]);
+        if user_data.Todo[list_index] == event {
+            user_data.Todo.remove(list_index);
+
+            let json = serde_json::to_string_pretty(&user_data).unwrap();
+            fs::write(file_path, json).expect("Unable to write to file");
+
+            println!("Successfully removed '{}' from {}'s todo list", event, user);
+            return format!("Successfully removed '{}' from {}'s todo list", event, user);
+        }
+    }
+
+    println!("The event '{}' doesn't exist inside of {}'s todo list", event, user);
+    return format!("The event '{}' doesn't exist inside of {}'s todo list", event, user);
 }
 
 // functions to implement
-fn delete_event(user: &str, event: &str) {}
 fn edit_event(user: &str, current_event: &str, new_event: &str) {}
 fn create_new_user(user: &str) {}
 fn delete_user(user: &str) {}
@@ -55,4 +80,5 @@ fn rocket() -> _ {
     rocket::build().mount("/", routes![say_hello])
                    .mount("/", routes![get_all_events])
                    .mount("/", routes![add_event])
+                   .mount("/", routes![delete_event])
 }
